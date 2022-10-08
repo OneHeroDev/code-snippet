602e307df66002d637623734791639bcd293c1e9
Rahul Ranjan <ranjan.rahul937@gmail.com>
2022-10-08 15:46:56 +0530

    add string_formatting

---
+++ Python/string_formatting.py
@@ -0,0 +1,13 @@
+language = "Python"
+
+# Method 1
+print(language + " is my favourite programming language.")
+
+# Method 2
+print(f"I code in {language}")
+
+# Method 3
+print("%s is very easy to learn." % (language))
+
+# Method 4
+print("I like the {} programming language.".format(language))
